2985e797ab44c14c28e37d20b048a3995537267d
CodedMarc <doramm4408@gmail.com>
2022-10-12 16:51:12 -0500

    Netlify Form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,19 +1,9 @@
 import { Box, Button, Input, Stack, Textarea } from "@chakra-ui/react";
 import { useState } from "react"
 import styles from '../styles/Contact.module.scss';
+import ContactForm from "./ContactForm";
 const Contact = () => {
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
-  const [error, setError] = useState('');
   const [submitted, setSubmitted] = useState(false);
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    const isEmail = email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-    if (name.length > 0 && isEmail && message.length > 0) return setSubmitted(true);
-    if (name.length === 0 || message.length === 0 || email.length === 0) return setError('Missing Input!');
-    if (!isEmail) return setError('Invalid Email');
-  }
   return (
     <section id='Contact'>
       <h1 className={styles.title}>Contact Me</h1>
@@ -26,53 +16,8 @@ const Contact = () => {
           </h2>
         </div>
         <div className={styles.contactForm}>
-        <h1 className={styles.submittedForm} style={submitted ? {display: 'block'} : {display: 'none'}}>Submitted!</h1>
-        <form className={styles.form} style={submitted ? {display: 'none'} : {display: 'block'}} name="contact" method="POST" data-netlify="true">
-            <input type="hidden" name="form-name" value="contact" />
-            <Stack spacing={4}>
-              <Input
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              name="Full Name"
-              required
-                placeholder="Full Name"
-                bg={'gray.100'}
-                border={0}
-                color={'gray.500'}
-                _placeholder={{
-                  color: 'gray.500',
-                }}
-              />
-              <Input
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              type="email"
-              required
-              name="Email"
-                placeholder="Email Address"
-                bg={'gray.100'}
-                border={0}
-                color={'gray.500'}
-                _placeholder={{
-                  color: 'gray.500',
-                }}
-              />
-              <Textarea
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              name="Message" 
-              required
-              placeholder="Enter Message" 
-              bg='gray.100' 
-              border={0} 
-              color={'gray.500'}
-              _placeholder={{color: 'gray.500'}}/>
-              <Button type="submit" onClick={handleSubmit} fontFamily={'heading'} bg={'gray.200'} color={'gray.800'}>
-                Submit
-              </Button>
-              <p style={{color: 'yellow', textAlign: 'center'}} className={styles.errorMessage}>{error}</p>
-            </Stack>
-          </form>
+          <h1 className={styles.submittedForm} style={submitted ? {display: 'block'} : {display: 'none'}}>Submitted!</h1>
+          <ContactForm submitted={submitted} func={setSubmitted}/>
         </div>
       </div>
     </section>
---
+++ components/ContactForm.tsx
@@ -0,0 +1,67 @@
+import { useState }from 'react'
+import styles from '../styles/Contact.module.scss';
+import { Button, Input, Stack, Textarea } from "@chakra-ui/react";
+const ContactForm = (props: any) => {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState('');
+  const {submitted, func} = props;
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const isEmail = email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if (name.length > 0 && isEmail && message.length > 0) return func(true);
+    if (name.length === 0 || message.length === 0 || email.length === 0) return setError('Missing Input!');
+    if (!isEmail) return setError('Invalid Email');
+  }
+  return (
+    <form className={styles.form} style={submitted ? {display: 'none'} : {display: 'block'}} name="contact" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="contact" />
+      <Stack spacing={4}>
+        <Input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        name="FullName"
+        required
+          placeholder="Full Name"
+          bg={'gray.100'}
+          border={0}
+          color={'gray.500'}
+          _placeholder={{
+            color: 'gray.500',
+          }}
+        />
+        <Input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        type="email"
+        required
+        name="Email"
+          placeholder="Email Address"
+          bg={'gray.100'}
+          border={0}
+          color={'gray.500'}
+          _placeholder={{
+            color: 'gray.500',
+          }}
+        />
+        <Textarea
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        name="Message" 
+        required
+        placeholder="Enter Message" 
+        bg='gray.100' 
+        border={0} 
+        color={'gray.500'}
+        _placeholder={{color: 'gray.500'}}/>
+        <Button type="submit" onClick={handleSubmit} fontFamily={'heading'} bg={'gray.200'} color={'gray.800'}>
+          Submit
+        </Button>
+        <p style={{color: 'yellow', textAlign: 'center'}} className={styles.errorMessage}>{error}</p>
+      </Stack>
+      </form>
+  )
+}
+
+export default ContactForm
